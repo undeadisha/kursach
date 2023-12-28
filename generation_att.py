3f76931681118ffbad21534e8fa9f297774db4fa
undeadisha <jlokis03@gmail.com>
2023-12-28 16:10:52 +0300

    parameters

--- generation_att.py
+++ generation_att.py
@@ -11,3 +11,13 @@ warnings.filterwarnings("ignore", category=UserWarning)
 # Загрузка словаря (vocab.pkl), который содержит отображение слов к их идентификаторам
 with open('vocab.pkl', 'rb') as f:
     words = pickle.load(f)
+
+# Параметры модели
+feature_dim = 576
+lstm_dim = 1024
+embed_dim = 1024
+attention_dim = 2048
+num_hidden = 256
+num_steps= 20
+dict_length=len(words)
+batch_size = 100
